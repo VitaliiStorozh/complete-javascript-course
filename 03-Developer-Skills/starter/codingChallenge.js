17a1bcbd963fd4951d1aee1162979054382ce432
Vitalii Storozh <storozh.softserve@gmail.com>
2023-01-09 19:15:26 +0200

    Developer skils

---
+++ 03-Developer-Skills/starter/codingChallenge.js
@@ -0,0 +1,7 @@
+'use strict';
+
+// 1) Understanding the problem
+// - With 2 arrays, should we implement functionality twice? NO! Just merge two arrays
+
+// 2) Breaking up into sub-problems
+// - Merge 2 arrays
