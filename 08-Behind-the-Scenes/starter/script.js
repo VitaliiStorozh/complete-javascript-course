51c080ab1fe7b6ac3b5658f9f073a99f9fb38321
Vitalii Storozh <storozh.softserve@gmail.com>
2023-01-19 15:25:04 +0200

    Scoping praktice

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,37 @@
 'use strict';
+
+// Scoping
+function calcAge(birthYear) {
+  const age = 2023 - birthYear;
+
+  function printAge() {
+    let output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (1981 <= birthYear && birthYear <= 1996) {
+      // Creating NEW variable with same name as outer scope's variable
+      const firstName = 'Mike';
+
+      // Reasssigning outer scope's variable
+      output = 'NEW OUTPUT!';
+
+      var millenial = true;
+      const str = `Oh, you are millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    // console.log(str);
+    console.log(millenial);
+    console.log(output);
+    // console.log(add(2, 3));
+  }
+
+  printAge();
+  return age;
+}
+
+const firstName = 'Vitalii';
+calcAge(1985);
