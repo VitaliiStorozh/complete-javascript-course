43493f6741dced6d06ff4cfd7c03ea3296cb6266
Vitalii Storozh <storozh.softserve@gmail.com>
2023-01-19 22:30:29 +0200

    regular function vs arrow function

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 /*
+///////////////////////////////////////
 // Scoping
 function calcAge(birthYear) {
   const age = 2023 - birthYear;
@@ -49,6 +50,7 @@ var me = 'Vitalii';
 let job = 'Engineer';
 const year = 1985;
 
+///////////////////////////////////////
 // Function
 console.log(addDecl(2, 3));
 // console.log(addExpr(2, 3));
@@ -73,4 +75,88 @@ var numProducts = 10;
 function deleteShopingCart() {
   console.log('All Products deleted!');
 }
+
+
+///////////////////////////////////////
+// The this Keyword in Practice
+console.log(this);
+
+const calcAge = function (birthYear) {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+calcAge(1991);
+
+const calcAgeArrow = birthYear => {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+calcAgeArrow(1980);
+
+const jonas = {
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+};
+jonas.calcAge();
+
+const matilda = {
+  year: 2017,
+};
+
+matilda.calcAge = jonas.calcAge;
+matilda.calcAge();
+
+const f = jonas.calcAge;
+f();
 */
+
+///////////////////////////////////
+// Regular functions vs Arrow functions
+
+// var firstName = 'Matilda';
+
+const vitalii = {
+  firstName: 'Vitalii',
+  year: 1985,
+  calcAge: function () {
+    // console.log(this);
+    console.log(2023 - this.year);
+
+    // Solution 1
+    // const self = this; // self or that
+    // const isMillenial = function () {
+    //   console.log(self);
+    //   console.log(self.year >= 1981 && self.year <= 1996);
+    // };
+
+    // Solution 2
+    const isMillenial = () => {
+      console.log(this);
+      console.log(this.year >= 1981 && this.year <= 1996);
+    };
+    isMillenial();
+  },
+  greet: () => {
+    // console.log(this);
+    console.log(`Hey ${this.firstName}`);
+  },
+};
+vitalii.greet();
+vitalii.calcAge();
+
+// arguments keyword
+const addExpr = function (a, b) {
+  console.log(arguments);
+  return a + b;
+};
+addExpr(2, 5);
+addExpr(2, 5, 8, 12);
+
+var addArrow = (a, b) => {
+  console.log(arguments);
+  return a + b;
+};
+addArrow(2, 5, 8);
