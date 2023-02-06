bd120df0b181ef77d8a8802cecc2e17b392345b3
Vitalii Storozh <storozh.softserve@gmail.com>
2023-02-06 09:28:59 +0200

    How Passing Arguments Works: Value vs. Reference

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -25,4 +25,45 @@ createBooking('LH123', 2, 800);
 createBooking('LH123', 3);
 createBooking('LH123', 5);
 createBooking('LH123', undefined, 1000);
+
+
+////////////////////////////////////////
+// How Passing Arguments Works: Value vs. Reference
+
+const flight = 'LH234';
+const vitalii = {
+  name: 'Vitalii Storozh',
+  passport: 239845793846702,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 239845793846702) {
+    alert('Checked in!');
+  } else {
+    alert('Wrong Passport!');
+  }
+};
+
+// checkIn(flight, vitalii);
+// console.log(flight);
+// console.log(vitalii);
+
+// Is the same as doing...
+// const flightNum = flight; // Primitive
+// const passenger = vitalii; // Reference
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 1000000000000);
+};
+
+newPassport(vitalii);
+checkIn(flight, vitalii);
 */
+
+////////////////////////////////////////
+// First-Class and Higher-Order Functions
+
+// Functions Accepting Callback Functions
