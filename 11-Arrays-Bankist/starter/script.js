6d2d4b04b6ca6957309b3f64a8badfcef8870ec6
Vitalii Storozh <storozh.softserve@gmail.com>
2023-03-05 00:20:09 +0200

    Simple Array Methods

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -73,4 +73,58 @@ const currencies = new Map([
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
+/*
 /////////////////////////////////////////////////
+// Simple Array Methods
+
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+// SLICE (does not mutate original arr)
+console.log(arr.slice(2));
+console.log(arr.slice(2, 4));
+console.log(arr.slice(-2));
+console.log(arr.slice(-1));
+console.log(arr.slice(1, -2));
+console.log(arr.slice());
+console.log([...arr]);
+
+// SPLICE (mutate original arr)
+// console.log(arr.splice(2));
+arr.splice(-1);
+console.log(arr);
+arr.splice(1, 2);
+console.log(arr);
+arr.splice(1, 0, 'arr', 'brr');
+console.log(arr);
+
+// REVERSE (mutate original arr)
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse());
+console.log(arr2);
+
+// CONCAT (does not mutate original arr)
+
+const letters = arr.concat(arr2);
+console.log(letters);
+console.log([...arr, ...arr2]);
+
+// JOIN (does not mutate original arr)
+console.log(letters.join(' - '));
+
+
+/////////////////////////////////////////////////
+// The new at Method
+
+const arr = [23, 45, 13];
+console.log(arr[0]);
+console.log(arr.at(0));
+
+// Getting array last element
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1));
+
+console.log('vitalii'.at(0));
+console.log('vitalii'.at(-1));
+*/
