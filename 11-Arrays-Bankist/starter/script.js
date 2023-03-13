6e052751fad824d0ab87770f8d377f34954dc318
Vitalii Storozh <storozh.softserve@gmail.com>
2023-03-13 11:58:16 +0200

    The map Method

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -236,3 +236,33 @@ const checkDogs = function (dogsJulia, dogsKate) {
 
 checkDogs(dogsJulia, dogsKate);
 */
+
+///////////////////////////////////////
+// Data Transformations: map, filter, redu  ce
+
+// The map Method
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const euroToUsd = 1.1;
+
+// const movementsUSD = movements.map(function (mov) {
+//   return mov * euroToUsd;
+// });
+
+const movementsUSD = movements.map(mov => mov * euroToUsd);
+
+console.log(movements);
+console.log(movementsUSD);
+
+const movementsUSDfor = [];
+for (const mov of movements) movementsUSDfor.push(mov * euroToUsd);
+console.log(movementsUSDfor);
+
+const movementDescriptions = movements.map(
+  (mov, i) =>
+    `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(
+      mov
+    )}`
+);
+console.log(movementDescriptions);
