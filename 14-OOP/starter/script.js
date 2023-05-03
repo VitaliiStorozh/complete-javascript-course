a5dcbf13d0bcde70c1ceb90810abef0e9c59bb94
Vitalii Storozh <storozh.softserve@gmail.com>
2023-05-03 21:52:32 +0300

    Inheritance Between "Classes":Constructor Function

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -177,3 +177,34 @@ const sarah = Object.create(PersonProto);
 sarah.init('Sarah', 1999);
 console.log(sarah);
 */
+
+/*
+///////////////////////////////////////
+// Inheritance Between "Classes": Constructor Functions
+
+const Person = function (firstName, birthYear) {
+  // Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+Person.prototype.calcAge = function () {
+  console.log(2023 - this.birthYear);
+};
+
+const Student = function (firstName, birthYear, course) {
+  Person.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+// Linking prototypes
+Student.prototype = Object.create(Person.prototype);
+
+Student.prototype.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+
+const mike = new Student('Mike', 2000, 'Computer Science');
+mike.introduce();
+mike.calcAge();
+*/
